16de8944ec78072ac15bcd87f83ef1250b369155
o_omi1217 <whdudal1217@naver.com>
2023-04-09 03:38:08 +0900

    List Articles

--- src/main/java/cho/o/me/blog/article/application/ArticleService.java
+++ src/main/java/cho/o/me/blog/article/application/ArticleService.java
@@ -1,20 +1,27 @@
 package cho.o.me.blog.article.application;
 
 import cho.o.me.blog.article.domain.Article;
+import cho.o.me.blog.article.repository.ArticleRepository;
+import cho.o.me.blog.article.repository.ArticleSpecifications;
 import cho.o.me.blog.article.ui.request.ArticleRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 
-import java.util.List;
-
+@RequiredArgsConstructor
 public class ArticleService {
-    public List<Article> articles(ArticleRequest request) {
 
+    ArticleRepository articleRepository;
+
+    public Page<Article> articles(ArticleRequest request) {
         Integer offset = request.getOffset();
         Integer limit = request.getLimit();
-
         Pageable pageable = PageRequest.of(offset,limit);
 
-        return null;
+        Specification<Article> spec = ArticleSpecifications.search(request);
+
+        return articleRepository.findAll(spec, pageable);
     }
 }
--- src/main/java/cho/o/me/blog/article/ui/ArticleController.java
+++ src/main/java/cho/o/me/blog/article/ui/ArticleController.java
@@ -4,9 +4,10 @@ import cho.o.me.blog.article.application.ArticleService;
 import cho.o.me.blog.article.domain.Article;
 import cho.o.me.blog.article.ui.request.ArticleRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,8 +19,7 @@ public class ArticleController {
     ArticleService articleService;
 
     @GetMapping("/api/articles")
-    public ResponseEntity<List<Article>> articles(@RequestParam ArticleRequest request){
-        List<Article> articles = articleService.articles(request);
-        return null;
+    public ResponseEntity<Page<Article>> articles(@ModelAttribute ArticleRequest request){
+        return ResponseEntity.ok(articleService.articles(request));
     }
 }
