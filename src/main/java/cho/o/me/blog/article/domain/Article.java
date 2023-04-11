1001f9acd96d18f314fe32932479a743331aec21
o_omi1217 <whdudal1217@naver.com>
2023-04-12 02:44:41 +0900

    아티클 엔터티 추가

--- src/main/java/cho/o/me/blog/article/domain/Article.java
+++ src/main/java/cho/o/me/blog/article/domain/Article.java
@@ -1,6 +1,20 @@
 package cho.o.me.blog.article.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Article {
+    @Id
+    private Long id;
     private String slug;
     private String title;
     private String description;
@@ -9,4 +23,5 @@ public class Article {
     private String createdAt;
     private String updatedAt;
     private String author;
+
 }
