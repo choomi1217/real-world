40150e64e0cbbb0f39676868ccdf76afdb6d0eff
o_omi1217 <whdudal1217@naver.com>
2023-04-12 02:46:02 +0900

    h2디비 커넥션 테스트

--- src/test/java/cho/o/me/blog/BlogApplicationTests.java
+++ src/test/java/cho/o/me/blog/BlogApplicationTests.java
@@ -1,13 +1,15 @@
 package cho.o.me.blog;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
 class BlogApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
