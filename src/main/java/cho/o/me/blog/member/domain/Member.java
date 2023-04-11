e0ed80d7f9c75f80e88f7f26bbf983976b4d7914
o_omi1217 <whdudal1217@naver.com>
2023-04-12 02:43:48 +0900

    멤버 업데이트 수정

--- src/main/java/cho/o/me/blog/member/domain/Member.java
+++ src/main/java/cho/o/me/blog/member/domain/Member.java
@@ -1,5 +1,6 @@
 package cho.o.me.blog.member.domain;
 
+import cho.o.me.blog.account.ui.request.UpdateRequest;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
@@ -19,4 +20,11 @@ public class Member {
     private String username;
     private String bio;
     private String image;
+
+    public void update(UpdateRequest updateRequest) {
+        this.email = updateRequest.getEmail();
+        this.bio = updateRequest.getBio();
+        this.image = updateRequest.getImage();
+        this.username = updateRequest.getUsername();
+    }
 }
--- src/main/java/cho/o/me/blog/member/repository/MemberRepository.java
+++ src/main/java/cho/o/me/blog/member/repository/MemberRepository.java
@@ -6,5 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findByEmail(String email);
 
-    Member update(String email, Member byEmail);
 }
