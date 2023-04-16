d7dcdb29288256035ebc33fdcaa5c75441712ed9
o_omi1217 <whdudal1217@naver.com>
2023-04-16 23:12:27 +0900

    스프링 시큐리티 컨피그

--- src/main/java/cho/o/me/blog/account/ui/request/LoginRequest.java
+++ src/main/java/cho/o/me/blog/account/ui/request/LoginRequest.java
@@ -1,13 +1,20 @@
 package cho.o.me.blog.account.ui.request;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
+@Setter
 public class LoginRequest {
     @NotBlank
     private String email;
     @NotBlank
     private String password;
+
+    public LoginRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 }
--- src/main/java/cho/o/me/blog/config/SecurityConfig.java
+++ src/main/java/cho/o/me/blog/config/SecurityConfig.java
@@ -1,14 +1,25 @@
 package cho.o.me.blog.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfig {
-    @Bean
-    public PasswordEncoder passwordEncoder(){
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private AccountUserDetailService accountUserDetailService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .csrf().disable()
+                .cors().disable()
+                .userDetailsService(accountUserDetailService)
+                .formLogin().disable()
+                .authorizeHttpRequests()
+        ;
     }
 }
