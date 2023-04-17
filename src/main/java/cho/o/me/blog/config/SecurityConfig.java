f42650b8cf25b06d9cea97536af4a7836edd6984
o_omi1217 <whdudal1217@naver.com>
2023-04-17 23:08:20 +0900

    스프링 시큐리티

--- src/main/java/cho/o/me/blog/config/SecurityConfig.java
+++ src/main/java/cho/o/me/blog/config/SecurityConfig.java
@@ -1,9 +1,14 @@
 package cho.o.me.blog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -11,15 +16,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private AccountUserDetailService accountUserDetailService;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic()
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.httpBasic()
                 .and()
                 .csrf().disable()
                 .cors().disable()
                 .userDetailsService(accountUserDetailService)
                 .formLogin().disable()
                 .authorizeHttpRequests()
-        ;
+                .antMatchers("/api/users/login").permitAll()
+                .anyRequest().authenticated()
+                .and().build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
