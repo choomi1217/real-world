85fcc9b8f45bb25af9e1c181b228feffdbd172c1
o_omi1217 <whdudal1217@naver.com>
2023-04-17 23:08:35 +0900

    비즈니스 로직과 구현 분리

--- src/main/java/cho/o/me/blog/account/ui/AccountController.java
+++ src/main/java/cho/o/me/blog/account/ui/AccountController.java
@@ -1,6 +1,6 @@
 package cho.o.me.blog.account.ui;
 
-import cho.o.me.blog.account.application.AccountService;
+import cho.o.me.blog.account.application.AccountUsercase;
 import cho.o.me.blog.account.ui.request.AccountRequest;
 import cho.o.me.blog.account.ui.request.LoginRequest;
 import cho.o.me.blog.account.ui.request.UpdateRequest;
@@ -17,28 +17,28 @@ import java.net.URISyntaxException;
 @RestController
 public class AccountController {
 
-    AccountService accountService;
+    AccountUsercase accountUsercase;
     @PostMapping("/api/users/login")
     public ResponseEntity<AccountResponse> login(@Validated @RequestBody LoginRequest loginRequest){
-        return ResponseEntity.ok(accountService.login(loginRequest));
+        return ResponseEntity.ok(accountUsercase.login(loginRequest));
     }
 
     @GetMapping("/api/user")
     public ResponseEntity<AccountResponse> user(@AuthenticationPrincipal UserDetails userDetails){
-        return ResponseEntity.ok(accountService.user(userDetails.getUsername()));
+        return ResponseEntity.ok(accountUsercase.user(userDetails.getUsername()));
     }
 
     @PostMapping("/api/users")
     public ResponseEntity<AccountResponse> register(@Validated @RequestBody AccountRequest request) throws URISyntaxException {
         return ResponseEntity.created(new URI("/api/user"))
-                .body(accountService.register(request));
+                .body(accountUsercase.register(request));
     }
 
     @PutMapping("/api/user")
     public ResponseEntity<AccountResponse> update(
               @AuthenticationPrincipal UserDetails userDetails
             , @Validated @RequestBody UpdateRequest updateRequest){
-        return ResponseEntity.ok(accountService.update(userDetails.getUsername(), updateRequest));
+        return ResponseEntity.ok(accountUsercase.update(userDetails.getUsername(), updateRequest));
     }
 
 }
