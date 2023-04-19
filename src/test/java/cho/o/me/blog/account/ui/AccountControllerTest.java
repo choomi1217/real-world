f08b24f4567d3bbf1608ec643a34dc434ec5c6e0
o_omi1217 <whdudal1217@naver.com>
2023-04-19 23:54:22 +0900

    콘텐트 타입 추가

--- src/test/java/cho/o/me/blog/account/ui/AccountControllerTest.java
+++ src/test/java/cho/o/me/blog/account/ui/AccountControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -18,9 +19,7 @@ class AccountControllerTest {
 
     @Autowired
     MockMvc mockMvc;
-
     ObjectMapper objectMapper = new ObjectMapper();
-
 
     @Test
     void login() throws Exception {
@@ -30,6 +29,7 @@ class AccountControllerTest {
         String content = objectMapper.writeValueAsString(request);
 
         mockMvc.perform(post("/api/users/login")
+                        .contentType(MediaType.APPLICATION_JSON)
                         .content(content)
                 ).andExpect(status().isOk())
                 .andExpect(jsonPath("$.user.email").value(email))
