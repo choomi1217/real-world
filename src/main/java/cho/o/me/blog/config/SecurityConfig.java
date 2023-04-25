8ec76996491727f1a2693b2d1bc1189fc31c4a56
o_omi1217 <whdudal1217@naver.com>
2023-04-25 23:17:27 +0900

    스프링 시큐리티

--- src/main/java/cho/o/me/blog/config/SecurityConfig.java
+++ src/main/java/cho/o/me/blog/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package cho.o.me.blog.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,14 +8,14 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@RequiredArgsConstructor
+public class SecurityConfig {
 
-    private AccountUserDetailService accountUserDetailService;
+    private final AccountUserDetailService accountUserDetailService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
