a69c388d85ed9999a10bbddf2b5de3671f95de60
o_omi1217 <whdudal1217@naver.com>
2023-04-28 05:52:22 +0900

    AccountRequest class > record

--- src/main/java/cho/o/me/blog/account/ui/request/AccountRequest.java
+++ src/main/java/cho/o/me/blog/account/ui/request/AccountRequest.java
@@ -1,17 +1,12 @@
 package cho.o.me.blog.account.ui.request;
 
+import cho.o.me.blog.account.domain.Account;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
-@Getter
-public class AccountRequest {
-
-    @NotBlank
-    private String username;
-    @NotBlank
-    private String email;
-    @NotBlank
-    private String password;
+public record AccountRequest(String email, String username, String password){
 
 }
