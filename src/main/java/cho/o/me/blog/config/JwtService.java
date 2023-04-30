97d60c085c571db713f86c42b7c4acd8ce44d5d4
o_omi1217 <whdudal1217@naver.com>
2023-04-30 21:29:43 +0900

    토큰 인증 방식 변경

---
+++ src/main/java/cho/o/me/blog/config/JwtService.java
@@ -0,0 +1,44 @@
+package cho.o.me.blog.config;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.UUID;
+
+@Service
+public class JwtService {
+    private static final String SECRET_KEY = "mXGi8&9cLq!dEW`m^9VN?R9cGl~z=&03?vb#-RCP|yW4*-dikEAt%{8SE*%yNLU";
+
+    public String emailToToken(String email) {
+        SecretKey key = generateKey();
+        return Jwts.builder()
+                .setId(UUID.randomUUID().toString())
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(oneHourLater())
+                .signWith(key)
+                .compact();
+    }
+
+    public String tokenToEmail(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(SECRET_KEY)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    private SecretKey generateKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8));
+    }
+
+
+    public Date oneHourLater(){
+        return new Date(new Date().getTime() + 3600000);
+    }
+}
