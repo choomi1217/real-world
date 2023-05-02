ed789e002c3738bd36e0b8c7330be529898a0320
o_omi1217 <whdudal1217@naver.com>
2023-05-02 23:32:05 +0900

    테스트시 매번 중복되는 어카운트 가입 및 로그인 로직만 따로 분리

--- src/test/java/cho/o/me/blog/account/AccountStep.java
+++ src/test/java/cho/o/me/blog/account/AccountStep.java
@@ -5,28 +5,55 @@ import cho.o.me.blog.account.ui.response.AccountResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcPrint;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-@SpringBootTest
-@AutoConfigureMockMvc
 public class AccountStep {
 
-    @Autowired
-    MockMvc mockMvc;
+    private final MockMvc mockMvc;
+    private final ObjectMapper mapper = new ObjectMapper();
 
-    public AccountResponse registUser(AccountRequest request, ObjectMapper mapper) throws Exception {
-        String requestAsString = mapper.writeValueAsString(request);
+    private final String TEST_EMAIL = "john@john.com", TEST_USERNAME = "john", TEST_PASSWORD = "JohnPark";
+
+    public AccountStep(MockMvc mockMvc) {
+        this.mockMvc = mockMvc;
+    }
+
+    public AccountRequest signUp(){
+        return new AccountRequest(TEST_EMAIL, TEST_USERNAME, TEST_PASSWORD);
+    }
+
+    public AccountRequest signUp(String email, String username, String password){
+        return new AccountRequest(email, username, password);
+    }
+
+    public AccountResponse login() throws Exception {
+
+        AccountRequest accountRequest = signUp();
 
         String responseAsString = mockMvc.perform(post("/api/users")
                 .contentType(MediaType.APPLICATION_JSON)
-                .content(requestAsString)
+                .content(mapper.writeValueAsString(accountRequest))
         ).andReturn().getResponse().getContentAsString();
 
         return mapper.readValue(responseAsString, AccountResponse.class);
     }
 
+    public AccountResponse login(AccountRequest accountRequest) throws Exception {
+
+        String responseAsString = mockMvc.perform(post("/api/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(accountRequest))
+        ).andReturn().getResponse().getContentAsString();
+
+        return mapper.readValue(responseAsString, AccountResponse.class);
+    }
+
+    public AccountResponse signUpAndLogin(String email, String username, String password) throws Exception {
+        return login(signUp(email,username,password));
+    }
 }
