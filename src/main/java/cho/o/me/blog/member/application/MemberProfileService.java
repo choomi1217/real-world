0a7a3d7addbef06eeef4beda9352396997ef81c4
o_omi1217 <whdudal1217@naver.com>
2023-05-04 23:10:40 +0900

    멤버 프로필 조회 기능

---
+++ src/main/java/cho/o/me/blog/member/application/MemberProfileService.java
@@ -0,0 +1,15 @@
+package cho.o.me.blog.member.application;
+
+import cho.o.me.blog.member.ui.response.ProfileResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberProfileService {
+    public ProfileResponse profile(String followerEmail , String followedName) {
+        return null;
+    }
+
+    public ProfileResponse profile(String username) {
+        return null;
+    }
+}
