3729e6105e047758ac1588314c40887a8a476819
o_omi1217 <whdudal1217@naver.com>
2023-05-04 23:00:00 +0900

    프로필과 팔로우를 멤버 컨트롤러에 통합

--- src/main/java/cho/o/me/blog/member/ui/MemberController.java
+++ src/main/java/cho/o/me/blog/member/ui/MemberController.java
@@ -1,23 +1,21 @@
 package cho.o.me.blog.member.ui;
 
 import cho.o.me.blog.exception.NotFollowingException;
-import cho.o.me.blog.follow.application.FollowService;
 import cho.o.me.blog.member.application.MemberFollowService;
+import cho.o.me.blog.member.application.MemberProfileService;
 import cho.o.me.blog.member.ui.response.ProfileResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-public class FollowController {
+public class MemberController {
 
     private final MemberFollowService memberFollowService;
+    private final MemberProfileService memberProfileService;
 
     @PostMapping(path = "/api/profiles/{username}/follow")
     public ResponseEntity<ProfileResponse> follow(
@@ -32,4 +30,15 @@ public class FollowController {
             , @PathVariable String username) throws NotFollowingException {
         return ResponseEntity.ok(memberFollowService.unfollow(userDetails.getUsername(), username));
     }
+    @GetMapping(path = "/api/profiles/{username}")
+    public ResponseEntity<ProfileResponse> profile(
+            @AuthenticationPrincipal(expression = "isAuthenticated() ? principal : null") UserDetails userDetails
+            , @PathVariable String username){
+        if(userDetails != null){
+            return ResponseEntity.ok(memberProfileService.profile(userDetails.getUsername(), username));
+        }else{
+            return ResponseEntity.ok(memberProfileService.profile(username));
+        }
+    }
+
 }
