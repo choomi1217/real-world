4266dacd2b6609b7eb57bc0c8a626d3a7f94ac7e
o_omi1217 <whdudal1217@naver.com>
2023-05-15 23:55:40 +0900

    생성자 변경

--- src/main/java/cho/o/me/blog/article/ui/response/ArticleResponse.java
+++ src/main/java/cho/o/me/blog/article/ui/response/ArticleResponse.java
@@ -1,11 +1,13 @@
 package cho.o.me.blog.article.ui.response;
 
 import cho.o.me.blog.article.domain.Article;
+import cho.o.me.blog.tag.domain.Tag;
 import com.fasterxml.jackson.annotation.JsonRootName;
-import lombok.Getter;
+import lombok.*;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Getter
 @JsonRootName("article")
@@ -17,15 +19,11 @@ public class ArticleResponse {
     String body;
     List<String> tagList;
 
-    public ArticleResponse(Article article) {
-        this.title = article.getTitle();
-        this.slug = article.getSlug();
-        this.description = article.getDescription();
-        this.body = article.getBody();
-        this.tagList = getTagNames(article.getTagList());
-    }
-
-    private List<String> getTagNames(List<UUID> tagList) {;
-        return null;
+    public ArticleResponse(String title, String slug, String description, String body, List<Tag> tagList) {
+        this.title = title;
+        this.slug = slug;
+        this.description = description;
+        this.body = body;
+        this.tagList = tagList.stream().map(Tag::getName).collect(Collectors.toList());
     }
 }
