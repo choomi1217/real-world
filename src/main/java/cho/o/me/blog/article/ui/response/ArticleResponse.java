6d0b041e854752347a267c6691145e899bb1a8c5
o_omi1217 <whdudal1217@naver.com>
2023-05-21 16:13:26 +0900

    tagList > tagNameList 로 변경, @JsonProperty("tagList") 추가

--- src/main/java/cho/o/me/blog/article/ui/response/ArticleResponse.java
+++ src/main/java/cho/o/me/blog/article/ui/response/ArticleResponse.java
@@ -1,10 +1,8 @@
 package cho.o.me.blog.article.ui.response;
 
-import cho.o.me.blog.account.domain.Account;
-import cho.o.me.blog.account.ui.response.AccountResponse;
-import cho.o.me.blog.article.domain.Article;
 import cho.o.me.blog.member.ui.response.ProfileResponse;
 import cho.o.me.blog.tag.domain.Tag;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,7 +22,8 @@ public class ArticleResponse {
     String title;
     String description;
     String body;
-    List<String> tagList;
+    @JsonProperty("tagList")
+    List<String> tagNameList;
     Date createdAt;
     Date updatedAt;
     Boolean favorited;
@@ -47,7 +46,7 @@ public class ArticleResponse {
         articleResponse.title = title;
         articleResponse.description = description;
         articleResponse.body = body;
-        articleResponse.tagList = allById.stream().map(Tag::getName).collect(Collectors.toList());
+        articleResponse.tagNameList = allById.stream().map(Tag::getName).collect(Collectors.toList());
         articleResponse.createdAt = createdAt;
         articleResponse.updatedAt = updatedAt;
         articleResponse.favorited = favorited;
