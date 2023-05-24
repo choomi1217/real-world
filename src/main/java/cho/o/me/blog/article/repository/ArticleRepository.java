27fe145e0b3685f15d749174a4ac4e2b6d357822
o_omi1217 <whdudal1217@naver.com>
2023-05-24 23:56:41 +0900

    게시글 여러건 조회 기능

--- src/main/java/cho/o/me/blog/article/repository/ArticleRepository.java
+++ src/main/java/cho/o/me/blog/article/repository/ArticleRepository.java
@@ -2,11 +2,24 @@ package cho.o.me.blog.article.repository;
 
 import cho.o.me.blog.article.domain.Article;
 import cho.o.me.blog.article.ui.response.ArticleResponse;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface ArticleRepository extends JpaRepository<Article, Long>, JpaSpecificationExecutor<Article> {
+public interface ArticleRepository extends JpaRepository<Article, Long> {
     Optional<Article> findBySlug(String slug);
+
+    @Query("select Article from Article " +
+            "where(:author is null or author = :author) " +
+            "and (:tag is null or tagList = :tag) " +
+            "and (:favorited is null or favoritedList = :favorited)")
+    List<Article> findAll(@Param("author") String author,
+                          @Param("tag") String tag,
+                          @Param("favorited") String favorited,
+                          Pageable pageable);
 }
