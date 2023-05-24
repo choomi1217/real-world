ded021da8006161ff8cec98328fdbbac2a961d72
o_omi1217 <whdudal1217@naver.com>
2023-05-24 23:56:51 +0900

    게시글 여러건 조회 테스트

--- src/test/java/cho/o/me/blog/article/ui/ArticleControllerTest.java
+++ src/test/java/cho/o/me/blog/article/ui/ArticleControllerTest.java
@@ -2,9 +2,11 @@ package cho.o.me.blog.article.ui;
 
 import cho.o.me.blog.account.AccountStep;
 import cho.o.me.blog.account.ui.response.AccountResponse;
-import cho.o.me.blog.article.ui.request.CreateArticleRequest;
-import cho.o.me.blog.article.ui.response.CreateArticleResponse;
+import cho.o.me.blog.article.ArticleStep;
+import cho.o.me.blog.article.ui.request.ArticleCreateRequest;
+import cho.o.me.blog.article.ui.response.ArticleCreateResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,6 +19,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.Matchers.containsInAnyOrder;
@@ -39,7 +42,7 @@ class ArticleControllerTest {
     @BeforeEach
     public void setup() throws Exception {
         AccountStep accountStep = new AccountStep(mockMvc);
-        author = accountStep.signUpAndLogin("tom@gmail.com", "tom", "tom");
+        author = accountStep.signUp("tom@gmail.com", "tom", "tom");
     }
 
     @DisplayName("게시글 한 건을 등록할 수 있다.")
@@ -51,7 +54,7 @@ class ArticleControllerTest {
         String body = "body";
         List<String> tagList = List.of("tag1", "tag2");
 
-        CreateArticleRequest createArticleRequest = CreateArticleRequest.builder()
+        ArticleCreateRequest createArticleRequest = ArticleCreateRequest.builder()
                 .title(title)
                 .description(description)
                 .body(body)
@@ -76,26 +79,27 @@ class ArticleControllerTest {
     @Test
     public void articels() throws Exception {
         //given
-        CreateArticleResponse article = createArticle();
-        //when, then
-        mockMvc.perform(get("/api/articles/"+article.getSlug()))
-                .andExpect(status().isOk())
-                .andDo(print());
-    }
-
-    private CreateArticleResponse createArticle() throws Exception {
         String title = "How to train your dragon";
         String description = "Ever wonder how?";
         String body = "It takes a Jacobian";
         List<String> tagList = List.of("dragons", "training");
-
-        CreateArticleRequest createArticleRequest = CreateArticleRequest.builder()
+        ArticleCreateRequest createArticleRequest = ArticleCreateRequest.builder()
                 .title(title)
                 .description(description)
                 .body(body)
                 .tagNameList(tagList)
                 .build();
+        ArticleCreateResponse article = createArticle(createArticleRequest);
+        //when, then
+        mockMvc.perform(get("/api/articles/"+article.getSlug()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("title").value(title))
+                .andExpect(jsonPath("description").value(description))
+                .andExpect(jsonPath("body").value(body))
+                .andDo(print());
+    }
 
+    private ArticleCreateResponse createArticle(ArticleCreateRequest createArticleRequest) throws Exception {
         String articleContent = mapper.writeValueAsString(createArticleRequest);
 
         String contentAsString = mockMvc.perform(post("/api/articles")
@@ -103,12 +107,45 @@ class ArticleControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(articleContent)).andReturn().getResponse().getContentAsString();
 
-        return mapper.readValue(contentAsString, CreateArticleResponse.class);
+        return mapper.readValue(contentAsString, ArticleCreateResponse.class);
 
     }
 
-    //Feed Articles
     //List Articles
+    @DisplayName("게시글을 여러개 조회 할 수 있다.")
+    @Test
+    public void articles() throws Exception {
+        List<ArticleCreateResponse> testArticles = getFakeArticles();
+        mockMvc.perform(get("/api/articles")
+                        .header(HttpHeaders.AUTHORIZATION, "Token " + author.token())
+                        .param("limit", "10")
+                        .param("offset", "0"))
+                .andDo(print());
+    }
+
+    private List<ArticleCreateResponse> getFakeArticles() throws Exception {
+        ArticleStep articleStep = new ArticleStep(mockMvc);
+        List<ArticleCreateResponse> testArticles = new ArrayList<ArticleCreateResponse>();
+
+        for(int i=0; i<30; i++){
+            ArticleCreateRequest request = getCreateArticleRequest();
+            ArticleCreateResponse testArticle = articleStep.createTestArticle(request, author);
+            testArticles.add(testArticle);
+        }
+        return testArticles;
+    }
+
+    private static ArticleCreateRequest getCreateArticleRequest() {
+        Faker faker = new Faker();
+        String title = faker.book().title();
+        String description = faker.lorem().sentence(3);
+        String body = faker.lorem().sentence(20);
+        List<String> tagNameList = faker.lorem().words(2);
+        return new ArticleCreateRequest(title, description, body, tagNameList);
+    }
+
+
+    //Feed Articles
 
     //Update Article
     //Delete Article
