92806c86e29617efce10f933350cc6862c292a41
o_omi1217 <whdudal1217@naver.com>
2023-06-11 17:11:21 +0900

    생성자 추가

--- src/main/java/cho/o/me/blog/article/ui/response/ArticleListResponse.java
+++ src/main/java/cho/o/me/blog/article/ui/response/ArticleListResponse.java
@@ -6,4 +6,8 @@ public class ArticleListResponse {
     List<ArticleResponse> articles;
     Integer articlesCount;
 
+    public ArticleListResponse(List<ArticleResponse> articles, Integer articlesCount) {
+        this.articles = articles;
+        this.articlesCount = articlesCount;
+    }
 }
